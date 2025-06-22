f5e945315433a02001c1b59c398876f51fa2a390
samir0607 <samir.gupta987@gmail.com>
2025-06-22 17:19:04 +0530

    feat: enhance Admin Login button styling for better user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,12 @@ export default function Home() {
       <div>
         <h1 className="text-3xl font-bold mb-2">T&P Vault</h1>
         <p className="text-gray-600">Welcome. Please go to <code>/admin/login</code> to begin.</p>
-        <button onClick={()=> router.push('/admin/login')}>Admin Login</button>
+        <button 
+        onClick={()=> router.push('/admin/login')}
+        className="bg-red-800 text-white font-semibold px-6 py-2 rounded hover:bg-red-900 transition disabled:opacity-50"
+        >
+          Admin Login
+        </button>
       </div>
     </main>
   );
